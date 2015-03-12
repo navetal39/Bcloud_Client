1ba43ab1dbec4d78abfc1d2658be762f4ada8121
Nave Tal <navetal@gmail.com>
2015-03-13 01:44:58 +0200

    Started writing the part that deals with the files

--- Memory_Class.py
+++ Memory_Class.py
@@ -9,8 +9,27 @@ EVERYTHING
 import os, sys, zipfile, zlib
 
 class Memory(object):
-    pass
+    def __init__(self, root):
+        self.root = root
 
+    def get_last_updates(self, folder_type):
+        pass
+    
+    def get_files(self, folder_type, files_list):
+       pass
+
+    def update_files(self, folder_type, raw_data):
+        pass
+
+    def delete_files(self, folder_type, files_list):
+        for path in files:
+            try:
+                os.remove('{}/{}/{}'.format(self.root, folder_type, file_name))
+            except WindowsError, error:
+                if error.errno == 2:
+                    continue
+                else:
+                    raise # Shouldn't get here...
 '''
 Exciting. Satisfying. Period.
 .
