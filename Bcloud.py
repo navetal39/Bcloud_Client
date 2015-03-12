b3bdab430e15ccc5d160e836b90ccc7b43d470a4
Nave Tal <navetal@gmail.com>
2015-03-12 18:39:26 +0200

    Began writing the client-server com class
    Yes, it's called "Server" too. Deal with it.
    
    Also transfered the important files here as well.

---
+++ Bcloud.py
@@ -0,0 +1,14 @@
+# INFO: #
+# ===================================
+
+import os, sys, socket
+from COM import *
+from RECURRING_FUNCTIONS import *
+
+def run():
+    pass
+
+'''
+Exciting. Satisfying. Period.
+.
+'''
---
+++ COM.py
@@ -0,0 +1,10 @@
+# INFO: #
+# ===================================
+
+SYNC_IP = '127.0.0.1'
+SYNC_PORT = 5126
+
+'''
+Exciting. Satisfying. Period.
+.
+'''
