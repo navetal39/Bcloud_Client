7f8d22449a18b5e1fed4fd15db77848de0914996
Nave <navetal@gmail.com>
2015-03-22 14:49:36 +0200

    undid last feature
    deal with it.

--- Memory_Class.py
+++ Memory_Class.py
@@ -92,15 +92,7 @@ class Memory(object):
         for file_name in files_list:
             try:
                 os.remove('{}/{}/{}'.format(self.path, folder_type, file_name))
-                print 'removed {}/{}/{}'.format(self.path, folder_type, file_name)
-                folders = file_name.split('/'); actual_file = folders[-1]; folders.remove(actual_file)
-                while len(folders > 0):
-                    path = os.path.join(self.path, folder_type, folders)
-                    if os.listdir(path) == []: # There are no files in the top foler
-                        os.remove(path)
-                        top_folder = folders[-1]; folders.remove(top_folder)
-                    else: # There are files, the foler is needed
-                        break
+                print 'del: removed {}/{}/{}'.format(self.path, folder_type, file_name)
             except WindowsError, error:
                 if error.errno == 2:
                     continue
