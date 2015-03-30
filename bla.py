8e7fc5d5787dfc47b64ae5527d7994e59a4826f6
Nave Tal <navetal@gmail.com>
2015-03-30 17:45:53 +0300

    phase 1

---
+++ bla.py
@@ -0,0 +1,13 @@
+# INFO: #
+# To be set with the installing program
+# ===================================
+
+import os
+SYNC_IP = '127.0.0.1'
+SYNC_PORT = 5126
+FOLDERS_LOCATION = os.path.expanduser('~')
+
+'''
+Exciting. Satisfying. Period.
+.
+'''
