8bce2581f6e89084cd4128a1529d831c17575a78
Tamir SH <tamir2341@gmail.com>
2015-04-21 21:24:28 +0300

    tab fixes :angry:

--- crypto.py
+++ crypto.py
@@ -4,8 +4,8 @@ from Crypto.Hash import SHA256
 
 # Hash func: #
 def do_hash(mess):
-	''' hashes the message 'mess' in SHA256.
-	'''
+    ''' hashes the message 'mess' in SHA256.
+    '''
     hasher = SHA256.new()
     hasher.update(mess)
     return hasher.hexdigest()
@@ -16,8 +16,8 @@ PADD_CHAR = '?'
 
 # Ecryption funcs: #
 def encrypt(plaintext):
-	''' Encrypts the plaintext received.
-	'''
+    ''' Encrypts the plaintext received.
+    '''
     from crypto_extended import generate_key as KEY
     padded_plaintext = padd(plaintext)
     encryptor = AES.new(KEY())
@@ -26,8 +26,8 @@ def encrypt(plaintext):
     return encoded_ciphertext
 
 def decrypt(encoded_ciphertext):
-	''' Decrypts the Base64 encoded ciphertext received.
-	'''
+    ''' Decrypts the Base64 encoded ciphertext received.
+    '''
     from crypto_extended import generate_key as KEY
     decryptor = AES.new(KEY())
     ciphertext = encoded_ciphertext.decode("Base64") #Decode from base64, the reason for encoding is written above.^
@@ -36,16 +36,16 @@ def decrypt(encoded_ciphertext):
 
 ## Utill for encryption funcs: ##
 def padd(not_padded):
-	''' Pad the plaintext before it is encrypted, so it will work with the AES block cipher.
-	'''
+    ''' Pad the plaintext before it is encrypted, so it will work with the AES block cipher.
+    '''
     padding_amount = (BLOCK_SIZE - len(not_padded) % BLOCK_SIZE)
     padding = padding_amount * PADD_CHAR
     padded = not_padded + padding
     return padded
 
 def depadd(padded):
-	''' De-pad the plaintext efter it was decrypted.
-	'''
+    ''' De-pad the plaintext efter it was decrypted.
+    '''
     depadded = padded.strip(PADD_CHAR)
     return depadded
 
